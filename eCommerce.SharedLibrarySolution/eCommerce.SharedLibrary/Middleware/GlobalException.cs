04354444b00e31c49899169d8a31955649306573
Hrytsenko Artem <hrytsenko.artem@lll.kpi.ua>
2025-02-08 20:47:55 +0200

    feat: added middleware to handle exceptions globally

---
+++ eCommerce.SharedLibrarySolution/eCommerce.SharedLibrary/Middleware/GlobalException.cs
@@ -0,0 +1,70 @@
+﻿using eCommerce.SharedLibrary.Logs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text.Json;
+
+namespace eCommerce.SharedLibrary.Middleware
+{
+    public class GlobalException(RequestDelegate next)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            string message = "Sorry, internal server error occured. Kindly, try again.";
+            int statusCode = (int)HttpStatusCode.InternalServerError;
+            string title = "Error";
+
+            try
+            {
+                await next(context);
+
+                if (context.Response.StatusCode == StatusCodes.Status429TooManyRequests)
+                {
+                    title = "Warning";
+                    message = "Too many request made.";
+                    statusCode = StatusCodes.Status429TooManyRequests;
+                    await ModifyHeader(context, title, message, statusCode);
+                }
+
+                if (context.Response.StatusCode == StatusCodes.Status401Unauthorized)
+                {
+                    title = "Alert";
+                    message = "You are not authorized to access.";
+                    await ModifyHeader(context, title, message, statusCode);
+                }
+
+                if (context.Response.StatusCode == StatusCodes.Status403Forbidden)
+                {
+                    title = "Out of Access";
+                    message = "You are not allowed/required to access.";
+                    statusCode = StatusCodes.Status403Forbidden;
+                    await ModifyHeader(context, title, message, statusCode);
+                }
+            } 
+            catch (Exception ex)
+            {
+                LogException.LogExceptions(ex);
+                if (ex is TaskCanceledException || ex is TimeoutException)
+                {
+                    title = "Out if time";
+                    message = "Request timeout. Try again.";
+                    statusCode = StatusCodes.Status408RequestTimeout;
+                }
+
+                await ModifyHeader(context, title, message, statusCode);
+            }
+        }
+
+        private static async Task ModifyHeader(HttpContext context, string title, string message, int statusCode)
+        {
+            context.Response.ContentType = "Application/json";
+            await context.Response.WriteAsync(JsonSerializer.Serialize(new ProblemDetails
+            {
+                Detail = message,
+                Status = statusCode,
+                Title = title,
+            }), CancellationToken.None);
+            return;
+        }
+    }
+}
