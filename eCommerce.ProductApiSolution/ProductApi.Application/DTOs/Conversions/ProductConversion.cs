6dfd23e2d7f2e53f2b694341b527a1858d096a17
Hrytsenko Artem <hrytsenko.artem@lll.kpi.ua>
2025-02-09 19:48:06 +0200

    feat: added entity Product and dto with conversion

---
+++ eCommerce.ProductApiSolution/ProductApi.Application/DTOs/Conversions/ProductConversion.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Query.Internal;
+using ProductApi.Domain.Entities;
+
+namespace ProductApi.Application.DTOs.Conversions
+{
+    public static class ProductConversion
+    {
+        public static Product ToEntity(ProductDTO product) => new()
+        {
+            Id = product.Id,
+            Name = product.Name,
+            Quantity = product.Quantity,
+            Price = product.Price,
+        };
+
+        public static (ProductDTO?, IEnumerable<ProductDTO>?) FromEntity(Product product, IEnumerable<Product>? products)
+        {
+            if (product is not null || products is null)
+            {
+                var singleProduct = new ProductDTO(
+                    product!.Id,
+                    product.Name!,
+                    product.Quantity,
+                    product.Price
+                    );
+
+                return (singleProduct, null);
+            }
+
+            if (products is not null || product is null)
+            {
+                var _products = products!.Select(p => new ProductDTO(p.Id, p.Name, p.Quantity, p.Price)).ToList();
+
+                return (null, _products);
+            }
+
+            return (null, null);
+        }
+    }
+}
