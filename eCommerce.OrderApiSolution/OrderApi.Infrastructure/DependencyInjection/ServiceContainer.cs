5e59f7f731c255e104fa0dc8b0745c79fbf771b1
Hrytsenko Artem <hrytsenko.artem@lll.kpi.ua>
2025-02-11 22:51:25 +0200

    feat: added ServiceContainer to OrderApi

---
+++ eCommerce.OrderApiSolution/OrderApi.Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,27 @@
+﻿using eCommerce.SharedLibrary.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OrderApi.Application.Interfaces;
+using OrderApi.Domain.Entities;
+using OrderApi.Infrastructure.Data;
+using OrderApi.Infrastructure.Repositories;
+
+namespace OrderApi.Infrastructure.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+        public static IServiceCollection AddInfrastructureService(this IServiceCollection services, IConfiguration configuration)
+        {
+            SharedServiceContainer.AddSharedServices<OrderDbContext>(services, configuration, configuration["MySerilog:FileName"]!);
+            services.AddScoped<IOrder, OrderRepository>();
+            return services;
+        }
+
+        public static IApplicationBuilder UseInfrastructurePolicy(this IApplicationBuilder app)
+        {
+            SharedServiceContainer.UseSharedPolicies(app);
+            return app;
+        }
+    }
+}
