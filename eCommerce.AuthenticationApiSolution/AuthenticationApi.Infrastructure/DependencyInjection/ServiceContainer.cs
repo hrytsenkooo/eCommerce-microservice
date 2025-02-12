ca94fd214942658d37fca0bb7d2e1d5470465906
Hrytsenko Artem <hrytsenko.artem@lll.kpi.ua>
2025-02-12 22:26:08 +0200

    feat: added dependency injection for AuthenticationApi

---
+++ eCommerce.AuthenticationApiSolution/AuthenticationApi.Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,26 @@
+﻿using AuthenticationApi.Application.Interfaces;
+using AuthenticationApi.Infrastructure.Data;
+using AuthenticationApi.Infrastructure.Repositories;
+using eCommerce.SharedLibrary.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuthenticationApi.Infrastructure.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+        public static IServiceCollection AddInfrastructureService(this IServiceCollection services, IConfiguration configuration)
+        {
+            SharedServiceContainer.AddSharedServices<AuthenticationDbContext>(services, configuration, configuration["MySerilog:FileName"]!);
+            services.AddScoped<IUser, UserRepository>();
+            return services;
+        }
+
+        public static IApplicationBuilder UserInfrastructurePolicy(this IApplicationBuilder app)
+        {
+            SharedServiceContainer.UseSharedPolicies(app);
+            return app;
+        }
+    }
+}
